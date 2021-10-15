0fa42d09cf27aba87cccfe7902f80d9c3110eaa5
Phytolizer <kyle.coffey@tamu.edu>
2021-10-15 00:59:29 -0500

    maint

--- inc/monkey/token.hpp
+++ inc/monkey/token.hpp
@@ -1,12 +1,38 @@
 #pragma once
 
+#include <string>
 #include <string_view>
 
-namespace monkey::token
-{
-struct type
-{
+namespace monkey::token {
+struct TokenType {
   std::string_view value;
 };
 
-}    // namespace monkey::token
+struct Token {
+  TokenType type;
+  std::string literal;
+};
+
+constexpr TokenType kIllegal = {"ILLEGAL"};
+constexpr TokenType kEof = {"EOF"};
+
+constexpr TokenType kIdent = {"IDENT"};
+constexpr TokenType kInt = {"INT"};
+
+constexpr TokenType kAssign = {"="};
+constexpr TokenType kPlus = {"+"};
+
+constexpr TokenType kComma = {","};
+constexpr TokenType kSemicolon = {";"};
+
+constexpr TokenType kLParen = {"("};
+constexpr TokenType kRParen = {")"};
+constexpr TokenType kLBrace = {"{"};
+constexpr TokenType kRBrace = {"}"};
+
+constexpr TokenType kFunction = {"FUNCTION"};
+constexpr TokenType kLet = {"LET"};
+
+}  // namespace monkey::token
+
+std::ostream& operator<<(std::ostream& os, const monkey::token::TokenType& tt);
--- src/token.cpp
+++ src/token.cpp
@@ -1,2 +1,5 @@
 #include "monkey/token.hpp"
 
+std::ostream& operator<<(std::ostream& os, const monkey::token::TokenType& tt) {
+  return os << tt.value;
+}
